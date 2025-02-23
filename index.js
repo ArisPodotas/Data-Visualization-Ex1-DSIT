d20d15f128342e6df491782b12fa53e3d874e901
Aris Podotas <a.Podotas@gmail.com>
2025-02-23 11:34:12 +0200

    Added some elements and going to see that they work

---
+++ index.js
@@ -0,0 +1,14 @@
+// Adding a go to the top button that only shows up when you get down the page
+window.addEventListener(
+    'scroll', function() {
+        const item = document.getElementsByClassName('btn')[0]; // [0] to select and element object from the list
+        const scrollPosition = window.scrollY + window.innerHeight; // How much the user has scrolled (from the top)
+        const documentHeight = document.documentElement.scrollHeight; // Total page height
+        if (scrollPosition >= documentHeight * 0.90) {
+            item.classList.remove('hidden');
+        } else {
+            item.classList.add('hidden'); // Optional: to hide the element again when scrolling back up
+        }
+    }
+);
+
